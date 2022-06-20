41f6aa2c827c04eb40f735ac47bba24231435b11
mabahmani <aminbahmani10@gmail.com>
2022-06-20 19:50:08 +0430

    update notification ui

--- app/src/main/java/com/mabahmani/instasave/util/NotificationHelper.kt
+++ app/src/main/java/com/mabahmani/instasave/util/NotificationHelper.kt
@@ -6,6 +6,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -26,7 +28,7 @@ object NotificationHelper {
                 context.getString(R.string.live_stream_download_notification_channel_id),
                 context.getString(R.string.live_stream_download_notification_channel_name),
                 context.getString(R.string.live_stream_download_notification_channel_description),
-                NotificationManager.IMPORTANCE_LOW
+                NotificationManager.IMPORTANCE_HIGH
             )
         }
 
@@ -34,10 +36,10 @@ object NotificationHelper {
             context,
             context.getString(R.string.live_stream_download_notification_channel_id)
         )
-            .setSmallIcon(R.drawable.ic_logo)
+            .setSmallIcon(R.drawable.ic_circle_arrow_down)
             .setContentTitle(context.getString(R.string.app_name))
             .setContentText(context.getString(R.string.downloading_live_streams))
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(false)
             .addAction(
                 NotificationCompat.Action(
@@ -48,7 +50,11 @@ object NotificationHelper {
                         Intent(context, DownloadLiveStreamsService::class.java).apply {
                             action = STOP_FOREGROUND_SERVICE_ACTION
                         },
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT } else { PendingIntent.FLAG_UPDATE_CURRENT }
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                        } else {
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        }
                     )
                 )
             )
@@ -77,15 +83,15 @@ object NotificationHelper {
             context,
             context.getString(R.string.live_stream_download_notification_channel_id)
         )
-            .setSmallIcon(R.drawable.ic_logo)
+            .setSmallIcon(R.drawable.ic_circle_arrow_down)
             .setContentTitle(context.getString(R.string.app_name))
             .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-            .setDefaults(Notification.DEFAULT_SOUND)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
             .build()
 
         with(NotificationManagerCompat.from(context)) {
-            notify(LIVE_STREAM_COMPLETED_NOTIFICATION_ID ++, notification)
+            notify(LIVE_STREAM_COMPLETED_NOTIFICATION_ID++, notification)
         }
 
         return notification
@@ -103,6 +109,13 @@ object NotificationHelper {
             val channel = NotificationChannel(channelId, channelName, importance).apply {
                 description = channelDescription
             }
+            channel.setSound(
+                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build()
+            )
             val notificationManager: NotificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
